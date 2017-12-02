4ab404a1b745a1083cc0452f2b865d3abcae77f4
Antoine Cusset <acusset@clever-age.com>
2017-12-02 11:52:59 +0100

    exercice 3

---
+++ learnyounode/exercice-3.js
@@ -0,0 +1,11 @@
+/** Created by antoine */
+const fs = require('fs');
+
+lines =
+    fs
+        .readFileSync(process.argv[2])
+        .toString()
+        .split(/\n/)
+        .length - 1;
+
+console.log(lines);
